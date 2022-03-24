c71bc22fa2cf9cecac1e3451bcf6290798a605ff
BrianH <brianhwang29@gmail.com>
2022-03-25 08:44:42 +0900

    study attr

---
+++ src/views/studyAttr.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3 v-bind="$attrs">hello world!!!</h3>
+    <h5>test !!!!</h5>
+    <button>press me</button>
+</template>
+
+<script>
+export default{
+  inheritAttrs: false
+}
+</script>
+
+<script setup>
+// import { useAttrs } from "vue";
+
+// const attrs = useAttrs();
+</script>
+
+<style scoped>
+.green{
+  background-color: green;
+}
+</style>
