b4df8093e03e14a04042f247d8355458efb759fb
BrianH <brianhwang29@gmail.com>
2022-03-25 08:44:59 +0900

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
 import StudyProps from './views/chapter/studyProps.vue';
+import StudyAttr from './views/studyAttr.vue';
+
 const name = "Brian"; 
+
+async function pressed(info){
+  alert('hello from pressed function' + info);
+}
 </script>
 
 <template>
-  <StudyProps />
+  <StudyProps :name="name" @pressed="pressed" />
   <router-link to="/study/ref">refs!!</router-link>
   <router-view></router-view>
+  <br/><br/><br/><br/><br/><br/>
+  
+  <StudyAttr class="green"></StudyAttr>
 </template>
 
