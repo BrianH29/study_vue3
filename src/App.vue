ef272a189ed467c7ae7e92bf808a613a3989f674
BrianH <brianhwang29@gmail.com>
2022-03-24 12:08:17 +0900

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup>
-import {ref, isRef, unref} from "vue";
-
-let count = ref(1);
-
-function double(){
-  count.value = unref(count) * 2;
-  isRef(count) ? alert(0) : alert(1);
-}
-</script>
+<script setup></script>
 
 <template>
-  <h1>{{ count }}</h1>
-  <button @click="double">round up</button>
+  <router-link to="/study/ref">ref!</router-link>
+  <router-view></router-view>
 </template>
 
--- src/views/chapter/studyRef.vue
+++ src/views/chapter/studyRef.vue
@@ -1,9 +1,50 @@
-<script setup></script>
+<script setup>
+import { customRef } from "vue";
+
+// let count = ref(1);
+//
+// function double(){
+//   count.value = unref(count) * 2;
+//   isRef(count) ? alert(0) : alert(1);
+// }
+
+// const cube = reactive({
+//   length : 10,
+//   width: 20,
+//   height: 30,
+// })
+
+// const length = toRef(cube, 'length');
+// const {length, width, height} = toRefs(cube);
+function useDebouncedRef(value, delay = 1000) {
+  let timeout;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          value = newValue
+          trigger()
+        }, delay)
+      }
+    }
+  })
+}
+
+const test = useDebouncedRef('hello');
+
+function change(){
+  test.value = 'kevin';
+}
+</script>
 
 <template>
-  <div>
-    <h1>hello</h1>
-  </div>
+  <h1>{{ test }}</h1>
+  <button @click="change">PRESS</button>
 </template>
 
 
